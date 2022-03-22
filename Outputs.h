46139cf0c4cdcaa0a1b723c9989c5b42139b273c
guillaumeb <guillaume.burger@wandercraft.eu>
2022-03-22 17:47:05 +0100

    Add doc on the outputs functions

--- Outputs.h
+++ Outputs.h
@@ -11,14 +11,10 @@
 
 namespace outputs
 {
+// Get the current timestamp formatted as a string.
 std::string get_timestamp();
 
-void write_pointcloud_projected(
-    PointCloud pc,
-    std::string file_path,
-    MiscLib::Vector< std::pair< MiscLib::RefCountPtr< PrimitiveShape >, size_t > > shapes
-    );
-
+// Write the pointcloud extracted by the RANSAC algorithm in PLY format.
 void write_pointcloud(
     PointCloud pc,
     std::string file_path,
@@ -26,6 +22,15 @@ void write_pointcloud(
     bool keep_unassigned = true
     );
 
+// Write the pointcloud extracted by the RANSAC projecting the points on the surfaces.
+void write_pointcloud_projected(
+    PointCloud pc,
+    std::string file_path,
+    MiscLib::Vector< std::pair< MiscLib::RefCountPtr< PrimitiveShape >, size_t > > shapes
+    );
+
+// Write a new pointcloud compressed and meshed with the projection of the points to the bitmap on the identified
+// surfaces.
 void write_pointcloud_shapes(
     PointCloud pc,
     std::string file_path,
@@ -33,10 +38,12 @@ void write_pointcloud_shapes(
     RansacShapeDetector::Options ransacOptions
     );
 
+// Extract triangular faces from 4 points in the bitmap depending on their respective occupancy.
 void extract_faces(MiscLib::Vector< char >  const& bitmap, MiscLib::Vector<size_t> const& bitmap_indices,
                    size_t i1, size_t i2, size_t i3, size_t i4,
                    std::vector<std::vector<int>>& facesOut);
 
+// Class used to record the duration of different stages in an algorithm.
 class TimeRecorder
 {
 public:
@@ -48,6 +55,7 @@ public:
     std::vector<std::string> time_labels_;
 };
 
+// Write metadata in a yaml format about the parameters and results of the RANSAC algorithm
 void write_metadata(
     std::string file_path,
     int remaining,
